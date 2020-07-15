93546da1d7c5e9cc98df586e55fb3fad901aca5b
Andrey <example@gmail.com>
2020-07-15 19:16:43 +0300

    finish lesson 020(object)

--- home/Practice/018 script.js
+++ home/Practice/018 script.js
@@ -23,7 +23,7 @@ function start() {
     }
 }
 
-// start();
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -48,7 +48,7 @@ function rememberMyFilms() {
     }
 }
 
-// rememberMyFilms();
+rememberMyFilms();
 
 function detectPersonalLevel() {
     if (personalMovieDB.count < 10) {
@@ -62,7 +62,7 @@ function detectPersonalLevel() {
     }
 }
 
-// detectPersonalLevel();
+detectPersonalLevel();
 
 function showMyDB(hidden) {
     if (!hidden) {
---
+++ home/Practice/019 script_(callback).js
@@ -0,0 +1,26 @@
+"use strict";
+
+function first() {
+    //Do something
+    setTimeout(function() {console.log(1);}, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+
+
+function learnJS(lang, callback) {
+    console.log(`I learn: ${lang}`);
+    callback();
+}
+
+function done (){
+    console.log('я прошел этот урок!')
+}
+
+learnJS('JavaScript', done);
