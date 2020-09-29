8f4bc24ac466d2ebf61aa91a20364c0d98eae6d3
Andry <fessysys911@gmail.com>
2020-09-29 21:01:00 +0300

    task 056 fetch reqest todo

--- home/Practice/t038/js/script.js
+++ home/Practice/t038/js/script.js
@@ -285,4 +285,9 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 5000);
     }
 
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json));
+
+
 });
