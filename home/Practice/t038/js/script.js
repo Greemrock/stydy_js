1567b206a684a8022668b7524a098c784ffedb30
Andry <fessysys911@gmail.com>
2020-09-29 21:03:50 +0300

    task 056 fetch API post reqest

--- home/Practice/t038/js/script.js
+++ home/Practice/t038/js/script.js
@@ -285,9 +285,15 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 5000);
     }
 
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(json => console.log(json));
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        body: JSON.stringify({name: 'Alex'}),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json));
 
 
 });
