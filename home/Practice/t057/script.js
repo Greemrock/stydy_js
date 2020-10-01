6eab482a8eb29f99777a3df76f286e811a3ab754
Andry <fessysys911@gmail.com>
2020-10-01 20:29:22 +0300

    add .map

--- home/Practice/t057/script.js
+++ home/Practice/t057/script.js
@@ -2,13 +2,23 @@
 
 // filter
 
-const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemar'];    
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemar'];    
 
-// будем плучать имена, которые меньше, чем 5 символов 
+// // будем плучать имена, которые меньше, чем 5 символов 
 
-const shortNames =names.filter(function(name) {
-    return name.length < 5;
-});
+// const shortNames =names.filter(function(name) {
+//     return name.length < 5;
+// });
 
-console.log(shortNames);
+// console.log(shortNames);
+
+// map
+
+const answers = ['IvAn', 'AnnA', 'Hello'];
+
+//возвращает callback функцией item нижнего регистра
+
+const result = answers.map(item => item.toLocaleLowerCase());   
+
+console.log(result);
 
