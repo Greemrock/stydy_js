d3a974f3d1a361f0f89f905190514325cb16ce7f
Andry <fessysys911@gmail.com>
2020-10-02 21:57:49 +0300

    sorting arraay filter,every,some,reduce,map

--- home/Practice/t057/script.js
+++ home/Practice/t057/script.js
@@ -14,11 +14,52 @@
 
 // map
 
-let answers = ['IvAn', 'AnnA', 'Hello'];
+// let answers = ['IvAn', 'AnnA', 'Hello'];
 
 //возвращает callback функцией item нижнего регистра
 
-answers = answers.map(item => item.toLocaleLowerCase());   
+// answers = answers.map(item => item.toLocaleLowerCase());   
 
-console.log(answers);
+// console.log(answers);
 
+// every/some 
+// возвращают булиновые значения true or false
+
+// const some = [4, 'qwe', 'adsfgmjnbt'];
+
+// some - выводит true, когда хоть один элемент искомого формата
+
+// console.log(some.some(item => typeof(item) === 'number'));
+
+// every - выводит true, когда все элементы искомого формата
+
+// console.log(some.every(item => typeof(item) === 'number'));
+
+// reduce
+
+// const arr = [4, 5, 1, 3, 2, 6];
+
+// // третий аргумент метода reduce устанавливает начальное значение sum
+// //                         3       4
+// //                         4       5
+// //                         9       1
+// const res = arr.reduce((sum, current) => sum + current, 3);
+// console.log(res);
+
+// const arr = ['apple', 'pear', 'plum'];
+
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+const obj = {
+    ivan: 'persone',
+    ann: 'persone',
+    dog: 'animal',
+    cat: 'animal'
+};
+
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'persone')
+.map(item => item[0]);
+
+console.log(newArr);
